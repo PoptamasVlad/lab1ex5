760fa6963cec2d03833a3c8347533d083ea39fd0
Poptamas Vlad <poptamasvlad@gmail.com>
2022-11-25 20:32:33 +0200

    Add project files.

---
+++ lab1ex5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab1ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Se citesc doua numere de la tastatura, x,y. Scrieti un program care va afisa cele doua valori in ordine crescatoare.
+            • Exemplu: citim ,9,0 Afisam : 0 9 */
+
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if(x >= y)
+            {
+                Console.WriteLine(y + " " + x);
+            }
+            else
+            {
+                Console.WriteLine(x + " " + y);
+            }
+        }
+    }
+}
